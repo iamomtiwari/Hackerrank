3b5ead4f90bc3f586311c63b512fd2d71009ba8c
OM TIWARI <121350344+iamomtiwari@users.noreply.github.com>
2024-05-22 19:05:23 +0530

    Update Mutations.py

--- Python/Mutations.py
+++ Python/Mutations.py
@@ -1,5 +1,8 @@
 def mutate_string(string, position, character):
-    string = string[:5] + "k" + string[6:]
-    return string
+    return string[:position] + character + string[position+1:]
 
 if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
